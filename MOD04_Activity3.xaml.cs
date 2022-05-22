723aec9baa7f72ea510805d130a8857b002bd6f9
Xzyra Lapid <83936947+xzyra14@users.noreply.github.com>
2022-05-23 05:52:34 +0800

    Add files via upload

---
+++ MOD04_Activity3.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity3 : ContentPage
+    {
+        public Activity3()
+        {
+            InitializeComponent();
+
+            //lst.ItemsSource = new List<String>()
+            //{ 
+            //    // Collection of String
+            //    "One", "Two", "Three", "Four", "Five"
+            //};
+
+            lst.ItemsSource = new List<Contacts>()
+            {
+                new Contacts()
+                {
+                    Name = "Xzyra L. Lapid",
+                    Number = "09759843504"
+                },
+                new Contacts()
+                {
+                    Name = "Stephanie Lopez",
+                    Number = "09123456789"
+                },
+                new Contacts()
+                {
+                    Name = "Irvin Justin M. Naguit",
+                    Number = "09198765432"
+                },
+            };
+        }
+    }
+}
